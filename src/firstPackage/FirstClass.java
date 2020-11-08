54fdbd73e9379d16096e118ccf3156b3191ea5d7
AhmetYasin <gulecahmetyasin@gmail.com>
2020-11-08 11:09:21 -0500

    second commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -2,4 +2,8 @@ package firstPackage;
 
 public class FirstClass {
 
+	public static void main(String[] args) {
+		System.out.println("first commit from local master branch");
+		System.out.println("Second commit from local master");
+	}
 }
