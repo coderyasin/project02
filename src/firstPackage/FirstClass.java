3d444300f6a1915de1f46830e61d9d899f79b2ec
AhmetYasin <gulecahmetyasin@gmail.com>
2020-11-08 12:35:33 -0500

    fifth commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -10,6 +10,7 @@ public class FirstClass {
 		System.out.println("second commit from remote master branch");
 		System.out.println("third commit from remote master branch");
 		System.out.println("Here is a merge conflict I create from remote branch");
+		System.out.println("Here is another conflict from local master branch");
 		
 	}
 }
